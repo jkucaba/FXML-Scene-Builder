b233e8881a96e739145effc71d5111a8adebe156
Jakub <jakub.kucaba@gmail.com>
2023-10-25 09:05:26 +0200

    Pre-Constructed Controller

--- src/main/java/com/jakubku/fxml/fxmldemo/Controller.java
+++ src/main/java/com/jakubku/fxml/fxmldemo/Controller.java
@@ -1,24 +1,23 @@
 package com.jakubku.fxml.fxmldemo;
 
-import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class Controller implements Initializable {
-    @FXML
-    public Label count_lbl;
-    public int count;
+    public Label message_lbl;
+    public Button message_btn;
+    private final String message;
+    public Controller(String message){
+        this.message = message;
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        count = 0;
-        count_lbl.setText("Count: " + count);
-    }
-    @FXML
-    public void onIncrement(){
-        count++;
-        count_lbl.setText("Count: " + count);
+        message_btn.setOnAction(event -> message_lbl.setText("Message: " + message));
     }
 }
--- src/main/java/com/jakubku/fxml/fxmldemo/Main.java
+++ src/main/java/com/jakubku/fxml/fxmldemo/Main.java
@@ -11,6 +11,8 @@ public class Main extends Application {
         // Allow to load fxml
         FXMLLoader fxmlLoader = new FXMLLoader(
                 getClass().getResource("main.fxml"));
+        Controller controller = new Controller("Hello World!");
+        fxmlLoader.setController(controller);
         Scene scene = new Scene(fxmlLoader.load(), 500, 450);
         stage.setScene(scene);
         stage.setTitle("FXML");
